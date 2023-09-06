3796ab9b23a20e8b483257f06384f1849552b7c2
Opalo1189 <enriquemaestre11@gmail.com>
2023-09-06 17:49:12 -0500

    Reactive Form finalizado primera parte

--- Estudio-de-Formularios-Reactivos/src/app/profile-editor/profile-editor.component.ts
+++ Estudio-de-Formularios-Reactivos/src/app/profile-editor/profile-editor.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 
 @Component({
@@ -12,17 +14,23 @@ export class ProfileEditorComponent {
 
   constructor(private fb: FormBuilder) { }
 
-  profileForm = new FormGroup({
+  profileForm = this.fb.group({
 
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
+    name: this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: [''],
+    }),
 
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
 
   onSubmit() {
@@ -32,11 +40,21 @@ export class ProfileEditorComponent {
 
   updateProfile() {
     this.profileForm.patchValue({
-      firstName: 'Nancy',
+      name: {
+        firstName: 'Nancy',
+      },
       address: {
         street: '123 drew Street'
       }
     })
+  }
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
   }
 
 }
