8522f37a544d336dd37f8114dbbaa653a73c392d
Opalo1189 <enriquemaestre11@gmail.com>
2023-09-07 17:06:35 -0500

    Formularios reactivos.

--- Estudio-de-Formularios-Reactivos/src/app/app.module.ts
+++ Estudio-de-Formularios-Reactivos/src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { NombreEditorComponent } from './nombre-editor/nombre-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NombreEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    DynamicFormQuestionComponent
   ],
   imports: [
     BrowserModule,
